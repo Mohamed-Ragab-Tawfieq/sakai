21220da4bd3052eec811e0e72c35669e95b73c0c
Mohamed Ragab <mohamedragab.cs@gmail.com>
2025-08-28 00:44:44 +0200

    filters

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Sidebar } from "./shared/sidebar/sidebar";
 import { Header } from './shared/header/header';
 import { Login } from "./features/login/pages/login/login";
+import { Filters } from "./shared/filters/filters";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, Sidebar, Header, Login],
+    imports: [RouterOutlet, Sidebar, Header, Login, Filters],
     templateUrl: './app.html',
     styleUrl: './app.scss'
 })
