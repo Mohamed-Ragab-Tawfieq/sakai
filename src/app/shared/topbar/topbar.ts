6d8454af7d5dff0fe87760047c0e6aeb3b3e4537
Mohamed Ragab <mohamedragab.cs@gmail.com>
2025-09-04 11:17:27 +0200

    header rename

---
+++ src/app/shared/topbar/topbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-topbar',
+    imports: [NgbDropdownModule],
+    templateUrl: './topbar.html',
+    styleUrl: './topbar.scss'
+})
+export class Topbar {
+
+}
